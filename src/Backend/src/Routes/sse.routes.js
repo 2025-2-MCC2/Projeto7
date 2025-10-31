23d4192c6274bcc74095746fe7a45cc32c2176bf
[João Pedro] <jpsilva030107@gmail.com>
2025-10-31 20:48:13 -0300

    Update sse.routes.js

--- src/Backend/src/Routes/sse.routes.js
+++ src/Backend/src/Routes/sse.routes.js
@@ -1,9 +1,11 @@
 // src/Backend/src/routes/sse.routes.js
 import { Router } from "express";
 import { sseGroupHandler, sseGlobalHandler } from "../services/sse.js";
+import { requireAuth } from "../middlewares/requireAuth.js";
 
 const router = Router();
-router.get("/stream/grupos/:grupoId", sseGroupHandler);
-router.get("/stream/global",          sseGlobalHandler);
+
+router.get("/stream/grupos/:grupoId", requireAuth, sseGroupHandler);
+router.get("/stream/global", requireAuth, sseGlobalHandler);
 
 export default router;
