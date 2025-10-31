fc9236750f1f4d6844d8f8cc80f2bc98c60a77d4
[João Pedro] <jpsilva030107@gmail.com>
2025-10-31 18:19:27 -0300

    grupos n esta funcionando

--- src/Frontend/src/pages/Grupos/DoacaoGrupo.jsx
+++ src/Frontend/src/pages/Grupos/DoacaoGrupo.jsx
@@ -58,7 +58,7 @@ function Lightbox({ open, items, index, onClose, onPrev, onNext, onRemove, showR
  * --------------------------------------------------------------------------*/
 const GRUPOS_KEY = "grupos"; //
 const PERFIL_KEY = "perfil"; //
-const API_BASE = '/api'; // Base da API backend
+const API_BASE = '/api.js'; // Base da API backend
 const ACCEPT_ATTACH = "application/pdf,image/png,image/jpeg,image/jpg"; //
 
 // Helper para formatar data/hora
